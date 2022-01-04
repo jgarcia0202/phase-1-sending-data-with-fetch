81290f14e71865ba9797c07734e83026b5545021
Jose <jmgarcia0202@gmail.com>
2022-01-03 22:28:31 -0800

    completed assignment

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ const mainUrl = "http://localhost:3000/users"
  
 const bodyTag = document.querySelector('body')
 
-let dataBody = {
-    name: "Steve",
-    email: "steve@steve.com"
-}
-
-let configObjects = {
-    method: "Post",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify(dataBody)
-}
-
 function submitData() {
     return fetch(mainUrl, configObjects)
     .then(response => response.json())
@@ -29,4 +15,18 @@ function getID(object) {
 
 function errorFunc(error){
     document.body.innerHTML = error.message
+}
+
+let dataBody = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+let configObjects = {
+    method: "Post",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(dataBody)
 }
